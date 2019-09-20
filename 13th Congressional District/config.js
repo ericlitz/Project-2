1379b8294344108eb5665d4fffc4643b5271a7dd
agrybaus <46001758+agrybaus@users.noreply.github.com>
2019-09-19 19:51:47 -0500

    Add files via upload

---
+++ 13th Congressional District/config.js
@@ -0,0 +1,15 @@
+// API key
+const API_KEY = "pk.eyJ1IjoiYWdyeWJhdXMxMSIsImEiOiJjazA5cDhzNjYwYjR0M2hybHVvemZuYnR0In0.PqhDMxf16PPStZZ1OFIVjg";
+const attribution = `
+  Map data & copy;
+  <a href="https://www.openstreetmap.org/">
+    OpenStreetMap
+    </a>
+  contributors,
+    <a href="https://creativecommons.org/licenses/by-sa/2.0/">
+      CC-BY-SA
+    </a>
+    , Imagery ©
+  <a href="https://www.mapbox.com/">
+    Mapbox
+    </a>`;
